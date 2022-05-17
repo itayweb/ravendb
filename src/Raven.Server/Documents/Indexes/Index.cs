496b17b5e25c92d3b1939b47daae6b6aa3cbb565
Arkadiusz Palinski <arek@ravendb.net>
2022-05-17 15:11:30 +0200

    RavenDB-18640 Using cached value for extended memory info during CanContinueBatch because it was too expensive to calculate it each time. The value is going to be updated every 5 seconds. All indexes and ETLs will use the same cached value.

--- src/Raven.Server/Documents/Indexes/Index.cs
+++ src/Raven.Server/Documents/Indexes/Index.cs
@@ -4395,6 +4395,7 @@ namespace Raven.Server.Documents.Indexes
                     ref _currentMaximumAllowedMemory,
                     allocated,
                     _environment.Options.RunningOn32Bits,
+                    DocumentDatabase.ServerStore.Server.MetricCacher,
                     _logger,
                     out var memoryUsage) == false)
                 {
